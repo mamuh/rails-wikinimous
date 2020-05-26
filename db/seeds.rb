0ecbb5f3589a0fe0f191e2791d977ed359571ed3
Mariana Caramori <musardo@mail.com>
2020-05-26 16:24:15 -0300

    app is funcitonal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::Game.title,
+    content: Faker::Game.genre
+    )
+end
